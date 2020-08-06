ca2420c8baf7587af90384633e673d5fb694abaf
François Mentec <francois.mentec@alten.com>
2020-08-06 17:00:53 +0200

    Fetch Leagues Names

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
+const axios = require('axios')
 const express = require('express')
 const app = express()
 const port = 3000
 
 const pug = require('pug')
 
-
-// For leagues list http://api.pathofexile.com/leagues (care to remove SSF)
-const LEAGUES_NAMES = {
-  'challenge': 'Harvest',
-  'challengehc': 'Hardcore Harvest',
-  'standard': 'Standard',
-  'hardcore': 'Hardcore'
-}
-const LEAGUES_ALIAS = ['challenge', 'challengehc', 'standard', 'hardcore']
-const FETCH_INTERVAL = 10 * 60 * 1000
-
 const Trade = require('./src/trade.js')
 let trade = new Trade('searches.json')
 
 const Render = require('./src/render.js')
 let renders = {}
-for (let league of LEAGUES_ALIAS) {
-  renders[league] = new Render(LEAGUES_NAMES[league], trade)
-}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
@@ -41,7 +28,28 @@ async function fetchPrice () {
 
   setTimeout(fetchPrice, FETCH_INTERVAL)
 }
-fetchPrice()
+
+const LEAGUES_ALIAS = ['challenge', 'challengehc', 'standard', 'hardcore']
+let leaguesNames = null
+
+axios.get('http://api.pathofexile.com/leagues').then(res => {
+  leaguesNames = {
+    'standard': res.data[0].id,
+    'hardcore': res.data[1].id,
+    'challenge': res.data[4].id,
+    'challengehc': res.data[5].id
+  }
+
+  console.log('Leagues names fetched')
+
+  for (let league of LEAGUES_ALIAS) {
+    renders[league] = new Render(leaguesNames[league], trade)
+  }
+
+  fetchPrice()
+})
+
+const FETCH_INTERVAL = 10 * 60 * 1000
 
 app.use(express.static('public'))
 app.set('view engine', 'pug')
@@ -52,8 +60,12 @@ app.get('/', (req, res) => {
 
 app.get('/:league', (req, res) => {
   if (LEAGUES_ALIAS.includes(req.params.league)) {
-    if (renders[req.params.league].ready) res.render(__dirname + '/public/html/index.pug', { r: renders[req.params.league], league: req.params.league, fetchInterval: FETCH_INTERVAL })
-    else res.render(__dirname + '/public/html/not_ready.pug')
+    if (typeof renders[req.params.league] !== 'undefined' && renders[req.params.league].ready) {
+      res.render(__dirname + '/public/html/index.pug', {
+        r: renders[req.params.league],
+        league: req.params.league,
+        fetchInterval: FETCH_INTERVAL })
+    } else res.render(__dirname + '/public/html/not_ready.pug')
   } else {
     res.redirect('/challenge')
   }
